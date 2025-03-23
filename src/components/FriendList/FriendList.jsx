19e2ad94298f90598688d235f5b2c0c409cc1563
Inna-kv27 <inna.kvitka27@gmail.com>
2025-03-23 22:44:02 +0100

    start

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,21 @@
+import FriendListItem from '../FriendListItem/FriendListItem';
+import css from './FriendList.module.css';
+
+const FriendList = ({ friends }) => {
+  console.log(friends);
+  return (
+    <ul className={css.friendsList}>
+      {friends.map((friend) => (
+        <li key={friend.id}>
+          <FriendListItem
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default FriendList;
